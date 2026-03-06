224de7561161ec7730e9a1e3beb1b4edb2f3bc56
Razil Christian <razilchristian@gmail.com>
2026-03-06 18:56:57 +0530

    update file

--- services/stock_service.py
+++ services/stock_service.py
@@ -1,7 +1,18 @@
 import yfinance as yf
 
-def get_stock_data(symbol):
-    stock = yf.Ticker(symbol)
-    data = stock.history(period="1y")
 
-    return data
+def get_stock_data(symbol):
+
+    try:
+        stock = yf.Ticker(symbol)
+
+        data = stock.history(period="10yr")
+
+        if data.empty:
+            return None
+
+        return data
+
+    except Exception as e:
+        print("Stock data error:", e)
+        return None
